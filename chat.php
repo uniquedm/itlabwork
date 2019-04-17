9d3d86ef1f332efe46e6bac5b48289fec035129c
uniquedm <47504086+uniquedm@users.noreply.github.com>
2019-04-17 11:48:59 +0530

    Add files via upload

---
+++ chat.php
@@ -0,0 +1,15 @@
+<HTML>
+<TITLE>X8 Chat</TITLE>
+<BODY>
+	<FORM action="<?php $_PHP_SELF?>" method="post">
+		<input type="text" name="x" >
+ 		<input type="submit" value="Send">
+	</FORM>
+</BODY>
+<?php
+	if(isset($_POST["x"]))
+		echo "Sent:".$_POST["x"]."<br/>";
+	$myfile = fopen("http://192.168.7.112/message.txt", "r") or die("Unable to open file!");
+	echo "Message: ".fread($myfile,filesize("message.txt"));
+	fclose($myfile);
+?>
